0314b6611adb5d0074eaef99360a593571e1ec78
iamjack996 <iamjack996@gmail.com>
2018-04-18 17:54:54 +0800

    11

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,9 +2,24 @@
 
 @section('content_admin')
 
-<h1>TEST</h1>
-
-<p> JS test!</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">管理後台頁</div>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <p>使用者狀態</p>
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -10,11 +10,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>{{ $title or "AdminLTE Dashboard" }}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{ asset("bower_components/bootstrap/dist/css/bootstrap.min.css")}}">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{ asset("bower_components/font-awesome/css/font-awesome.min.css")}}">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{ asset("bower_components/Ionicons/css/ionicons.min.css")}}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/dist/css/AdminLTE.min.css")}}">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
